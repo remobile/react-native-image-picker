5984ab19f077eb5a2ccb2ca8b33e92526ab16325
fov42550564 <42550564@qq.com>
2017-03-30 14:05:53 +0800

    eslint code

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 'use strict';
 
-
-var ReactNative = require('react-native');
-var {
-    NativeModules
+const ReactNative = require('react-native');
+const {
+    NativeModules,
 } = ReactNative;
 
-var RCTImagePicker = NativeModules.ImagePicker;
+const RCTImagePicker = NativeModules.ImagePicker;
 
-var ImagePicker = {
-    getPictures:function(options, successCallback, errorCallback) {
-        options = options||{};
-        successCallback = successCallback || (()=>{});
-        errorCallback = errorCallback || (()=>{});
-        var params = {
+const ImagePicker = {
+    getPictures:function (options, successCallback, errorCallback) {
+        options = options || {};
+        successCallback = successCallback || (() => {});
+        errorCallback = errorCallback || (() => {});
+        const params = {
             maximumImagesCount: options.maximumImagesCount ? options.maximumImagesCount : 15,
             width: options.width ? options.width : 0,
             height: options.height ? options.height : 0,
-            quality: options.quality ? options.quality : 100
+            quality: options.quality ? options.quality : 100,
         };
         RCTImagePicker.getPictures([params], successCallback, errorCallback);
-    }
+    },
 };
 
 module.exports = ImagePicker;
